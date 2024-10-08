b6f914ec02c210d8c2cdba4a1588830ae2b0977b
BobWizardLad <bobwizardlad@Gmail.com>
2024-10-08 18:18:48 -0500

    Warning Random Tile
    I will telegraph tiles dissapearing with a sprite that is called into the scene onto the hex about to be destroyed.

--- level_handler.gd
+++ level_handler.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
 @export var current_map: TileMapLayer
+@export var warning_texture: CompressedTexture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	fade_tile(current_map)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# Function to make random tiles on the map fade to black
+func fade_tile(tileset: TileMapLayer):
+	var cell_coord = tileset.get_used_cells()[randi_range(0, tileset.get_used_cells().size()-1)]
+	var warn = Sprite2D.new()
+	warn.texture = warning_texture
+	warn.position = current_map.map_to_local(cell_coord)
+	add_child(warn)
+	
