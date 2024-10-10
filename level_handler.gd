242dc85ae393156bbb3aa54679675ae791cdb385
BobWizardLad <bobwizardlad@Gmail.com>
2024-10-10 13:38:22 -0500

    Tile Decay Speedup
    Tiles will now decay at a faster and faster pace as there are fewer and fewer tiles on the map.

--- level_handler.gd
+++ level_handler.gd
@@ -7,11 +7,21 @@ class_name LevelHandler
 @export var warning_texture: AtlasTexture
 
 var will_remove_tiles: bool = true
+var DECAY_TIME: float = 3.00
+var decay_speed: float = DECAY_TIME
 
 signal tile_removed
 
 func get_cell_local_to_map(coord: Vector2) -> Vector2i:
 	return current_map.local_to_map(coord)
+
+func _process(_delta):
+	if current_map.get_used_cells().size() < 15:
+		decay_speed = DECAY_TIME * 0.4
+	elif current_map.get_used_cells().size() < 30:
+		decay_speed = DECAY_TIME * 0.6
+	elif current_map.get_used_cells().size() < 50:
+		decay_speed = DECAY_TIME * 0.8
 
 func is_current_map_cell_exist(coord: Vector2i) -> bool:
 	if current_map.get_cell_source_id(coord) != -1:
@@ -30,7 +40,7 @@ func fade_tile(tileset: TileMapLayer):
 	warn.texture = warning_texture
 	warn.position = current_map.map_to_local(cell_coord)
 	add_child(warn)
-	tween.tween_property(warn, "modulate", Color(1.0, 1.0, 1.0, 1.0), 1.0)
+	tween.tween_property(warn, "modulate", Color(1.0, 1.0, 1.0, 1.0), decay_speed * 0.8)
 	tween.tween_callback(remove_tile.bind(tileset, cell_coord, warn))
 
 func remove_tile(tileset: TileMapLayer, tile_coord: Vector2i, warning: Sprite2D):
@@ -39,5 +49,6 @@ func remove_tile(tileset: TileMapLayer, tile_coord: Vector2i, warning: Sprite2D)
 	emit_signal("tile_removed")
 
 func _on_timer_timeout() -> void:
+	decay_timer.wait_time = decay_speed
 	if current_map.get_used_cells().size() > 0 and will_remove_tiles:
 		fade_tile(current_map)
